625647f3d72e84da17f5c766b1b670ac531ed0d9
AmanIslamGithub <157483321+AmanIslamGithub@users.noreply.github.com>
2024-01-23 17:12:21 +0600

    funny virus
    Make sure to carefully use it! First You need to plug in the usb port in to the device (android) and your pc or laptop! and type bash adb-reboot-cowsay-virus.sh The requirements are to install are cowsay, xcowsay, adb

---
+++ adb-reboot-cowsay-virus-plus.sh
@@ -0,0 +1,11 @@
+xcowsay Do you want to see magic?
+xcowsay OK!
+xcowsay Here we go!
+adb shell reboot -p
+xcowsay Tada!
+xcowsay Did i shutdown your phone?
+xcowsay In USB!
+xcowsay Hhahahah!
+cowsay Have Fun!!!
+echo If you havent installed it!
+echo The Requirements Are: cowsay, xcowsay, adb
